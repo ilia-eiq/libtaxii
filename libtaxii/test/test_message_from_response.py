c39307f4258435251bc3b9abf7b78c7b697a7776
Ilia Novoselov <i.novoselov@eclecticiq.com>
2026-02-27 11:35:45 +0100

    Test every content type

--- libtaxii/test/test_message_from_response.py
+++ libtaxii/test/test_message_from_response.py
@@ -1,54 +1,73 @@
+from http.client import HTTPResponse
 from urllib.error import HTTPError
 
+import pytest
+
 import libtaxii
+from libtaxii.constants import VID_TAXII_XML_10, VID_TAXII_XML_11, VID_CERT_EU_JSON_10
 import libtaxii.messages
 
 
 IN_RESPONSE_TO = "test in_response_to value"
 TAXII_CONTENT_TYPE = libtaxii.constants.VID_TAXII_XML_11
 TEST_MESSAGE_ID = "test message id"
-CONTENT_XML = '<taxii_11:Discovery_Request xmlns:taxii_11="http://taxii.mitre.org/messages/taxii_xml_binding-1.1" message_id="test message id"/>'
+
+MESSAGES = {
+    VID_TAXII_XML_10: (
+        '<taxii:Discovery_Request xmlns:taxii="http://taxii.mitre.org/messages/taxii_xml_binding-1" message_id="1"/>',
+        libtaxii.messages_10.DiscoveryRequest(message_id='1')
+    ),
+    VID_TAXII_XML_11: (
+        '<taxii_11:Discovery_Request xmlns:taxii_11="http://taxii.mitre.org/messages/taxii_xml_binding-1.1" message_id="test message id"/>',
+        libtaxii.messages_11.DiscoveryRequest(message_id='test message id'),
+    ),
+    VID_CERT_EU_JSON_10: (
+        '{"extended_headers": {}, "message_type": "Discovery_Request", "message_id": "1"}',
+        libtaxii.messages_10.DiscoveryRequest(message_id='1')
+    )
+}
 
 
-def test_httplib_http_response(httpserver):
+@pytest.mark.parametrize('taxii_content_type', MESSAGES.keys())
+def test_httplib_http_response(taxii_content_type, httpserver):
+    content, expected_message = MESSAGES[taxii_content_type]
+
     httpserver.expect_request("/poll_service_path/").respond_with_data(
-        CONTENT_XML.encode('windows-1252'),
+        content.encode('windows-1252'),
         content_type="application/xml; charset=windows-1252",
-        headers={"X-TAXII-Content-Type": TAXII_CONTENT_TYPE}
+        headers={"X-TAXII-Content-Type": taxii_content_type}
     )
 
     client = libtaxii.clients.HttpClient()
     http_response = client.call_taxii_service2(httpserver.host, '/poll_service_path/', libtaxii.constants.VID_TAXII_XML_10, b"", port=httpserver.port)
+    assert isinstance(http_response, HTTPResponse)
+
     message = libtaxii.get_message_from_httplib_http_response(http_response, IN_RESPONSE_TO)
-
-    assert isinstance(message, libtaxii.messages_11.DiscoveryRequest)
-    assert message.in_response_to is None
-    assert message.message_id == 'test message id'
+    assert message == expected_message
 
 
-def test_httplib_http_response_error(httpserver):
+@pytest.mark.parametrize('taxii_content_type', MESSAGES.keys())
+def test_httplib_http_response_error(taxii_content_type, httpserver):
+    content, expected_message = MESSAGES[taxii_content_type]
+
     httpserver.expect_request("/poll_service_path/").respond_with_data(
-        CONTENT_XML.encode('windows-1252'),
+        content.encode('windows-1252'),
         status=500,
         content_type="application/xml; charset=windows-1252",
-        headers={"X-TAXII-Content-Type": TAXII_CONTENT_TYPE}
+        headers={"X-TAXII-Content-Type": taxii_content_type}
     )
 
     client = libtaxii.clients.HttpClient()
     http_response = client.call_taxii_service2(httpserver.host, '/poll_service_path/', libtaxii.constants.VID_TAXII_XML_10, b"", port=httpserver.port)
-
     assert isinstance(http_response, HTTPError)
 
     message = libtaxii.get_message_from_httplib_http_response(http_response, IN_RESPONSE_TO)
-
-    assert isinstance(message, libtaxii.messages_11.DiscoveryRequest)
-    assert message.in_response_to is None
-    assert message.message_id == 'test message id'
+    assert message == expected_message
 
 
 def test_httplib_http_response_no_taxii_content_type(httpserver):
     httpserver.expect_request("/poll_service_path/").respond_with_data(
-        CONTENT_XML.encode('windows-1252'),
+        "some content".encode('windows-1252'),
         content_type="application/xml; charset=windows-1252",
     )
 
@@ -66,9 +85,9 @@ def test_httplib_http_response_no_taxii_content_type(httpserver):
         f'''Server: {server}\r\n'''
         f'''Date: {date}\r\n'''
         '''Content-Type: application/xml; charset=windows-1252\r\n'''
-        '''Content-Length: 129\r\n'''
+        '''Content-Length: 12\r\n'''
         '''Connection: close\r\n'''
         '''\r\n'''
-        '''<taxii_11:Discovery_Request xmlns:taxii_11="http://taxii.mitre.org/messages/taxii_xml_binding-1.1" message_id="test message id"/>'''
+        '''some content'''
     )
 
