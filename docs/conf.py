453695fbabd4d004bb203cad3e2a35baaf9df5b2
Ilia Novoselov <i.novoselov@eclecticiq.com>
2026-03-04 12:50:17 +0100

    Removed deprecated get_html_theme_path call

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,6 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if not on_rtd:
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 else:
     html_theme = 'default'
 
